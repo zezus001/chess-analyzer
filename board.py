a75a59dfe80cc28dbd18f4c92f932aa966bf7eb2
zezus001 <zezus001@gmail.com>
2025-12-17 22:17:51 +0100

    Added Piece class and piece movements

--- board.py
+++ board.py
@@ -1,8 +1,28 @@
 from constants import FILES, OPPOSITE_COLOR, PIECE_MOVEMENTS, PIECE_CAPTURES, SLIDING_PIECES
+
+class Piece():
+    def __init__(self, pieceType, pos, color, id):
+        self.type = pieceType
+        self.pos = pos
+        self.color = color
+        self.id = id # A unique identifier for piece lookup
+        self.slides = pieceType in SLIDING_PIECES
+        
 class Board():
     def __init__(self):
-        self.board = self.startPos()
-        self.pieces = {}
+        self.board = self.startPos() # Visual elements
+        self.pieces = {
+            'white': {},
+            'black': {}
+        }
+        self.pieceCounters = {
+            'pawn': 0,
+            'knight': 0,
+            'bishop': 0,
+            'rook': 0,
+            'queen': 0,
+            'king': 0
+        }
         self.legalMoves = {} # for later
         self.moveHistory = {}
         self.FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
@@ -23,10 +43,12 @@ class Board():
         ]
     
     def createPiece(self, pieceType, pos, color):
-        pass # define later
+        piece = Piece(pieceType, pos, color, f'{pieceType}_{self.pieceCounters[pieceType]}')
+        self.pieceCounters[pieceType] += 1
+        self.pieces[color][piece.id] = piece
 
     def startingPieces(self):
-        pass # use createPiece to initialize starting position
+        pass # todo: use createPiece to initialize starting position
 
 if __name__ == '__main__':
     board = Board()
--- constants.py
+++ constants.py
@@ -4,10 +4,51 @@ OPPOSITE_COLOR = {
     'white': 'black',
     'black': 'white'
 }
+SLIDING_PIECES = ('queen', 'bishop', 'rook')
+
+# rook: horizontal and vertical directions
+ROOK_MOVES = [
+    (1, 0),
+    (-1, 0),
+    (0, 1),
+    (0, -1)
+]
+
+# bishop: diagonals
+BISHOP_MOVES = [
+    (1, 1),   # down-right
+    (1, -1),  # down-left
+    (-1, 1),  # up-right
+    (-1, -1)  # up-left
+]
+
+# queen: rook + bishop
+QUEEN_MOVES = ROOK_MOVES + BISHOP_MOVES
+
+# knight: L-shapes
+KNIGHT_MOVES = [
+    (2, 1),
+    (2, -1),
+    (-2, 1),
+    (-2, -1),
+    (1, 2),
+    (1, -2),
+    (-1, 2),
+    (-1, -2)
+]
+
+# king: like queen but only 1 square
+KING_MOVES = QUEEN_MOVES  # Board logic will limit to 1 step
+
+# pawn moves: separate handling recommended
+# define only directions; distances handled in move logic
+PAWN_MOVES = [(1, 0)]  # forward, will invert based on color
+
 PIECE_MOVEMENTS = {
-    # fill later
+    "rook": ROOK_MOVES,
+    "bishop": BISHOP_MOVES,
+    "queen": QUEEN_MOVES,
+    "knight": KNIGHT_MOVES,
+    "king": KING_MOVES,
+    "pawn": PAWN_MOVES
 }
-PIECE_CAPTURES = {
-    # this too
-}
-SLIDING_PIECES = ['queen', 'bishop', 'rook']
